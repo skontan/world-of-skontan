5944be6b9635b4f2cc6853b17396ba9a9cde0c51
Skontan <skontan@gmail.com>
2022-09-03 19:41:40 +0200

    First commit

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { getFetch } from "@trpc/client";
+import { loggerLink } from "@trpc/client/links/loggerLink";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import { trpc } from "./utils/trpc";
+
+function AppContent() {
+  const { data } = trpc.useQuery(["hello"]);
+  const { mutate: createUserMutation } = trpc.useMutation("add_user", {
+    onSuccess(data, variables, context) {
+      console.log("Successfully created user:", { data });
+      console.log({ variables, context });
+    },
+  });
+
+  const handleClick = () => {
+    createUserMutation({
+      name: "Jonathan",
+      email: "skontan@gmail.com",
+      password: "Hej123",
+    });
+  };
+  return (
+    <div>
+      <p>{data?.message}</p>
+      <button onClick={handleClick}>Add user</button>
+    </div>
+  );
+}
+
+function App() {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 1000,
+          },
+        },
+      })
+  );
+  const url = "http://localhost:8000/api/trpc";
+  const links = [
+    loggerLink(),
+    httpBatchLink({
+      maxBatchSize: 10,
+      url,
+    }),
+  ];
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links,
+      fetch: async (input, init?) => {
+        const fetch = getFetch();
+        return fetch(input, {
+          ...init,
+          credentials: "include",
+        });
+      },
+    })
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <AppContent />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
+
+export default App;
