d83f0bcaaead71967152501d547473d0ff0693f9
Skontan <skontan@gmail.com>
2022-09-03 20:43:55 +0200

    Fix: server port env

--- packages/server/src/config/default.ts
+++ packages/server/src/config/default.ts
@@ -2,7 +2,7 @@ import path from "path";
 require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 
 const customConfig: { port: number; origin: string; dbUri: string } = {
-  port: 8000,
+  port: parseInt(process.env.PORT ?? "8000"),
   origin: process.env.ORIGIN as unknown as string,
 
   dbUri: process.env.DATABASE_URL as unknown as string,
